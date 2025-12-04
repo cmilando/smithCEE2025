7d8d779104a98420d10adeabc0884752052825ce
Chad Milando <44262671+cmilando@users.noreply.github.com>
2025-12-04 14:40:09 -0500

    update

---
+++ R/data.R
@@ -0,0 +1,49 @@
+#' Boston feature data
+#'
+#' For every gridcell for every time point, feature data
+#'
+#' @format tbd.
+#' \describe{
+#' \item{Cases}{Numeric}
+#' }
+#'
+#'
+"boston_data"
+
+#' Valid city names
+#'
+#' For now, just boston
+#'
+#' @format tbd.
+#' \describe{
+#' \item{Cases}{Numeric}
+#' }
+#'
+#'
+"valid_city_names"
+
+
+#' Weather station data
+#'
+#' For every gridcell for every time point, feature data
+#'
+#' @format tbd.
+#' \describe{
+#' \item{Cases}{Numeric}
+#' }
+#'
+#'
+"station_data"
+
+
+#' boston shapefile
+#'
+#' For every gridcell for every time point, feature data
+#'
+#' @format tbd.
+#' \describe{
+#' \item{Cases}{Numeric}
+#' }
+#'
+#'
+"boston_shp"
